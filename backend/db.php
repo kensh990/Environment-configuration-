431eb28ffad4611e8a6c83bd55aae1625927a6ab
kensh990 <kuja.zereso.ui@phinmaed.com>
2026-02-21 10:50:18 +0800

    Create db.php

---
+++ backend/db.php
@@ -0,0 +1,14 @@
+<?php
+require_once "config.php";
+
+$conn = new mysqli(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME']
+);
+
+if ($conn->connect_error) {
+    die("Database connection failed.");
+}
+?>
